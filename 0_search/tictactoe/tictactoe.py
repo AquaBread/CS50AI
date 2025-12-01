fa1ef354b390a5d2da0e51d2592a133f0ff99d86
AquaBread <brianpaniagua4@gmail.com>
2025-11-30 22:01:58 -0600

    tictactoe complete

--- 0_search/tictactoe/tictactoe.py
+++ 0_search/tictactoe/tictactoe.py
@@ -22,46 +22,169 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    x_count = 0
+    o_count = 0
+    for row in board:
+        for cell in row:
+            if cell == X:
+                x_count += 1
+            elif cell == O:
+                o_count += 1
+
+    if x_count <= o_count:
+        return X
+    else:
+        return O
+
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    
+    moves = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                moves.add((i, j))
+    return moves
+
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    
+    i, j = action
+
+    if i < 0 or i > 2 or j < 0 or j > 2:
+        raise Exception("Invalid action: out of bounds")
+
+    if board[i][j] is not EMPTY:
+        raise Exception("Invalid action: cell not empty")
+
+    new_board = [row[:] for row in board]
+    new_board[i][j] = player(board)
+
+    return new_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    # row winner
+    for row in board:
+        if row == [X, X, X]:
+            return X
+        if row == [O, O, O]:
+            return O
+        
+    # column winner
+    for j in range(3):
+        col = [board[i][j] for i in range(3)]
+        if col == [X, X, X]:
+            return X
+        if col == [O, O, O]:
+            return O
+
+    # diagonal winner
+    diag1 = [board[i][i] for i in range(3)]
+    diag2 = [board[i][2 - i] for i in range(3)]
+
+    for line in (diag1, diag2):
+        if line == [X, X, X]:
+            return X
+        if line == [O, O, O]:
+            return O
+        
+    return None
+
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    
+    if winner(board) is not None:
+        return True
+
+    for row in board:
+        for cell in row:
+            if cell == EMPTY:
+                return False
+
+    return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    
+    win = winner(board)
+    
+    if win == X:
+        return 1
+    elif win == O:
+        return -1
+    else:
+        return 0
 
+# Helper functions for minimax
+def max_value(board):
+    """
+    Returns the maximum utility value for the maxing player.
+    """
+    
+    if terminal(board):
+        return utility(board)
+    
+    v = -math.inf
+    for action in actions(board):
+        v = max(v, min_value(result(board, action)))
+    return v
+
+def min_value(board):
+    """
+    Returns the minimum utility value for the minning player.
+    """
+    
+    if terminal(board):
+        return utility(board)
+    
+    v = math.inf
+    for action in actions(board):
+        v = min(v, max_value(result(board, action)))
+    return v
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+
+    current_player = player(board)
+
+    if current_player == X:
+        best_value = -math.inf
+        best_action = None
+        for action in actions(board):
+            value = min_value(result(board, action))
+            if value > best_value:
+                best_value = value
+                best_action = action
+        return best_action
+    else:
+        best_value = math.inf
+        best_action = None
+        for action in actions(board):
+            value = max_value(result(board, action))
+            if value < best_value:
+                best_value = value
+                best_action = action
+        return best_action
